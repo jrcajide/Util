b7c89a02df79d9be05cc8fc9f63caa7623a773b9
Jose <jrcajide.analytics@gmail.com>
2014-12-18 09:36:05 +0100

    Import
    Get parameter from query string

---
+++ get parameter from query string.js
@@ -0,0 +1,17 @@
+//Get parameter from query string
+
+function getUrlParameter(sParam)
+{
+    var sPageURL = window.location.search.substring(1);
+    var sURLVariables = sPageURL.split('&');
+    for (var i = 0; i < sURLVariables.length; i++) 
+    {
+        var sParameterName = sURLVariables[i].split('=');
+        if (sParameterName[0] == sParam) 
+        {
+            return sParameterName[1];
+        }
+    }
+} 
+
+var ref = getUrlParameter('ref');
